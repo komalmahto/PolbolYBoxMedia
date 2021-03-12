068e4d62a0aaf9428689b70cc3e22c8dd981069b
devsts14 <devsts14@gmail.com>
2021-03-12 14:20:03 +0530

    S

--- src/Views/Awards/Youtube.jsx
+++ src/Views/Awards/Youtube.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Youtube = ({match}) => {
   return (
-    <div style={{padding:'3rem',height:'90vh'}}>
+    <div style={{height:'100vh'}}>
 <iframe src={`https://www.youtube.com/embed/${match.params.ytlink}?autoplay=1&mute=1`}
         frameborder='0'
         allow='autoplay; encrypted-media'
