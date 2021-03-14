7b9abc995d73bc7d7da4ddd72eb75804c1547af9
devsts14 <devsts14@gmail.com>
2021-03-14 18:37:20 +0530

    c1

--- src/Views/Awards/Youtube.jsx
+++ src/Views/Awards/Youtube.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Youtube = ({match}) => {
   return (
     <div style={{height:'100vh'}}>
-<iframe src={`https://www.youtube.com/embed/${match.params.ytlink}?autoplay=1&mute=1`}
+<iframe src={`https://www.youtube.com/embed/${match.params.ytlink}?autoplay=1&fullscreen=1&mute=0`}
         frameBorder='0'
-        allow='autoplay; encrypted-media'
+        allow='autoplay; encrypted-media; fullscreen'
         style={{height:'100%',width:'100%'}}
         allowFullScreen
         title='video'
